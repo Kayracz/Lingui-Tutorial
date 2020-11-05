f095e7f59160473f67ed1a2966be9537b5af5ed9
Kayra Czerniewicz <kayra@cuadds.com>
2020-11-05 12:17:31 -0400

    before try 2

--- src/LanguageSelector.js
+++ src/LanguageSelector.js
@@ -3,9 +3,8 @@ import React from "react";
 const LanguageSelector = ({ language, onChangeLangage }) => {
 	const handleChange = (event) => {
 		event.preventDefault();
-
 		onChangeLangage(event.target.value);
-	};
+	}
 
   // EVENTS AND SELECTION OPTIONS
 	return (
--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import { I18nProvider } from "@lingui/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+// This file is responsible for importing the translations of the selected language and rendering the whole webpage.
+
 async function loadMessages(language) {
+	return await import(`@lingui/loader!./locales/${language}/messages.json`);
 	// The keyword Await makes JavaScript wait until the promise returns a result.
 	// makes the async function block wait
-	return await import(`@lingui/loader!./locales/${language}/messages.json`);
 }
 
 // STATES
 const LocalizedApp = () => {
+	const [language, setLanguage] = useState("en");
 	const [catalogs, setCatalogs] = useState({});
-	const [language, setLanguage] = useState("es");
 
 	async function handleLanguageChange(language) {
 		const newCatalog = await loadMessages(language);
@@ -22,7 +24,6 @@ const LocalizedApp = () => {
 		setCatalogs(newCatalogs);
 		setLanguage(language);
 	}
-
 
 	return (
 		<BrowserRouter>
